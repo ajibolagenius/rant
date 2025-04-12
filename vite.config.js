c2daa7ba09a7e3e81e9d1e122d5f69d00f75f671
AJIBOLA (DON_GENIUS) AKELEBE <ajiboladolapogenius@gmail.com>
2025-04-12 23:25:13 +0100

    feat: Add animejs dependency and update Vite config for optimized imports

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     optimizeDeps: {
-        include: ['animejs/lib/anime.es.js'],
+        include: ['animejs'],
     },
 })
